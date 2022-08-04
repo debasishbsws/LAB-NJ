d6700ae6da74cbc0ad8c9b7faa5caaff646c45b2
Debasish Biswas <debasishbsws.abc@gmail.com>
2022-08-04 09:31:23 +0000

    Q2

---
+++ Lab2Q2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Lab2Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x0, y0;
+        System.out.print("Enter the center cordinates: ");
+        x0 = sc.nextInt();
+        y0 = sc.nextInt();
+
+        System.out.print("Enter the Radius of the circle: ");
+        int radius = sc.nextInt();
+
+        System.out.print("Enter the Arbitary points cordinates: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        double dist = Math.sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0));
+
+        if (dist <= radius) {
+            System.out.println("Point is inside the circle.");
+        } else {
+            System.out.println("Point is outside the circle.");
+        }
+    }
+}
