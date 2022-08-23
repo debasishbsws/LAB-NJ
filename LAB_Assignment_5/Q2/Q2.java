02b39661d3d3c1704a5fda7d678c73bda242a5ff
Debasish Biswas <debasishbsws.abc@gmail.com>
2022-08-23 15:01:23 +0530

    Q2

--- LAB_Assignment_5/Q2/Q2.java
+++ LAB_Assignment_5/Q2/Q2.java
@@ -3,8 +3,21 @@ package LAB_Assignment_5.Q2;
 import java.util.Scanner;
 
 public class Q2 {
-    Scanner sc = new Scanner(System.in);
-    float a, b;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a, b;
+        System.out.print("Enter nunbers: ");
+        a = sc.nextFloat();
+        b = sc.nextFloat();
+        sc.close();
+        try {
+            if ((double) a / b < 0.00001) {
+                throw new UserVerySmallNumException();
+            }
+        } catch (UserVerySmallNumException e) {
+            System.out.println(e);
+        }
+    }
 
 }
 
